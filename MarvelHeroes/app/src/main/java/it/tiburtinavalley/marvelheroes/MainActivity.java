0d16927f019497ceabc878b53faf05fad4fd035e
piercirocaliandro <piercirocaliandro998@gmail.com>
2020-06-19 23:17:41 +0200

    Added Creators and Event info
    #13

--- MarvelHeroes/app/src/main/java/it/tiburtinavalley/marvelheroes/MainActivity.java
+++ MarvelHeroes/app/src/main/java/it/tiburtinavalley/marvelheroes/MainActivity.java
@@ -6,11 +6,15 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -36,10 +40,9 @@ public class MainActivity extends AppCompatActivity {
         imgVolley = new ImageApiVolley(getApplicationContext());
     }
 
-    class Holder implements View.OnClickListener {
+    class Holder {
         final RecyclerView rvHeroes;
         final EditText etHeroSearch;
-        final Button btnSearch;
 
         public Holder() {
             this.rvHeroes = findViewById(R.id.rvHeroes);
@@ -60,16 +63,28 @@ public class MainActivity extends AppCompatActivity {
             };
 
             this.etHeroSearch = findViewById(R.id.etHeroSearch);
-            this.btnSearch = findViewById(R.id.btnSearch);
-            this.btnSearch.setOnClickListener(this);
+            this.etHeroSearch.setOnEditorActionListener(new ConfButtonListener());
         }
 
-        @Override
-        public void onClick(View v) {
-            if (v.getId() == R.id.btnSearch) {
-                String nameStartsWith = etHeroSearch.getText().toString();
-                volleyMarvel.getCharactersInfo(nameStartsWith);
+
+        public class ConfButtonListener  implements TextView.OnEditorActionListener {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    String nameStartsWith = etHeroSearch.getText().toString();
+                    volleyMarvel.getCharactersInfo(nameStartsWith);
+                    hideSoftKeyboard(MainActivity.this);
+                    return true;
+                }
+                else {
+                    return false;
+                }
             }
+        }
+
+        public void hideSoftKeyboard(Activity activity){
+            InputMethodManager imm = (InputMethodManager)activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), 0);
         }
     }
 
