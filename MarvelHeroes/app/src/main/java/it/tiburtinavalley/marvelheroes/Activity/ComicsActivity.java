15154ebd072da9c4d35ec8ec5b045dafbb00f57b
Robert Adrian Minut <44144508+adrianrob1@users.noreply.github.com>
2020-06-22 18:22:55 +0200

    fix: thumbnail not loading when opening comics pages and misaligned items in layouts

--- MarvelHeroes/app/src/main/java/it/tiburtinavalley/marvelheroes/Activity/ComicsActivity.java
+++ MarvelHeroes/app/src/main/java/it/tiburtinavalley/marvelheroes/Activity/ComicsActivity.java
@@ -1,22 +1,14 @@
 package it.tiburtinavalley.marvelheroes.Activity;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.List;
-
 import it.tiburtinavalley.marvelheroes.Model.Comics;
-import it.tiburtinavalley.marvelheroes.Model.Urls;
 import it.tiburtinavalley.marvelheroes.R;
 import it.tiburtinavalley.marvelheroes.UrlsRecyclerView;
 import it.tiburtinavalley.marvelheroes.Volley.ImageApiVolley;
@@ -26,33 +18,35 @@ public class ComicsActivity extends AppCompatActivity {
     private UrlsRecyclerView urlsAdapter;
 
     @Override
-    public void onCreate(Bundle savedIstanceState) {
-        super.onCreate(savedIstanceState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.comic_layout);
         comic = getIntent().getParcelableExtra("comic");
         Holder holder = new Holder();
     }
 
-    class Holder{
+    class Holder {
         private ImageView ivComicImage;
         private TextView tvComicName;
         private TextView tvPageCount;
         private RecyclerView rvUrls;
-        private  RecyclerView.LayoutManager layoutManagerUrls;
+        private RecyclerView.LayoutManager layoutManagerUrls;
 
-        public Holder(){
+        public Holder() {
             ivComicImage = findViewById(R.id.ivComicImg);
             tvComicName = findViewById(R.id.tvComicName);
             tvPageCount = findViewById(R.id.tvPageCount);
             rvUrls = findViewById(R.id.rvUrls);
-            setDatas();
+            setData();
         }
 
-        private void setDatas(){
+        private void setData() {
             ImageApiVolley imgVolley = new ImageApiVolley(getApplicationContext());
             imgVolley.addHeroImg(ivComicImage);
-            if(comic.getImages().size() > 0) {
-                imgVolley.getImageFromUrl(comic.getImages().get(0).getPath()+"."+comic.getImages().get(0).getExtension());
+            if (comic.getImages().size() > 0) {
+                imgVolley.getImageFromUrl(
+                        comic.getImages().get(0).getPath().replaceFirst("http", "https")
+                                + "." + comic.getImages().get(0).getExtension());
             }
             tvComicName.setText(comic.getTitle());
             tvPageCount.setText(comic.getPageCount());
