de76574adc236d8bf63e440636c472136f53f5dd
Gian Marco Falcone <gian.marco.1698@gmail.com>
2020-07-01 21:53:11 +0200

    added string no results

--- MarvelHeroes/app/src/main/java/it/tiburtinavalley/marvelheroes/volley/MarvelApiVolley.java
+++ MarvelHeroes/app/src/main/java/it/tiburtinavalley/marvelheroes/volley/MarvelApiVolley.java
@@ -108,6 +108,7 @@ public abstract class MarvelApiVolley implements Response.ErrorListener, Respons
     @Override
     public void onErrorResponse(VolleyError error) {
         ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        assert cm != null;
         NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
         if (activeNetwork != null && activeNetwork.isConnectedOrConnecting()) {
             ToastClass toast = new ToastClass(context);
@@ -135,7 +136,7 @@ public abstract class MarvelApiVolley implements Response.ErrorListener, Respons
                 } else {
                     if (showToast) {
                         ToastClass toast = new ToastClass(context);
-                        toast.showToast("No results has to been showed");
+                        toast.showToast(context.getString(R.string.msg_no_results));
                     }
                 }
                 fillList(heroesList);
