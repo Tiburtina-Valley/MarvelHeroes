8f6f4cb49cdd1a491d37d5ba818182daecb67db4
piercirocaliandro <piercirocaliandro998@gmail.com>
2020-07-01 13:10:41 +0200

    fixed contextual menu

--- MarvelHeroes/app/src/main/java/it/tiburtinavalley/marvelheroes/activity/MainActivity.java
+++ MarvelHeroes/app/src/main/java/it/tiburtinavalley/marvelheroes/activity/MainActivity.java
@@ -2,16 +2,16 @@ package it.tiburtinavalley.marvelheroes.activity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import it.tiburtinavalley.marvelheroes.fragment.FavouritesFragment;
 import it.tiburtinavalley.marvelheroes.fragment.HomeFragment;
 import it.tiburtinavalley.marvelheroes.fragment.SearchFragment;
@@ -19,7 +19,7 @@ import it.tiburtinavalley.marvelheroes.R;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    private FavouritesFragment frag;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,7 +41,10 @@ public class MainActivity extends AppCompatActivity {
                     return true;
 
                 case R.id.action_favourites:
-                    setFragment(new FavouritesFragment());
+                    FavouritesFragment favFrag = new  FavouritesFragment();
+                    frag = favFrag;
+                    setFragment(favFrag);
+
                     return true;
 
                 case R.id.action_search:
