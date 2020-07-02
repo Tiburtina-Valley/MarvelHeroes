25d9af0ce179eb0e405e95f2c7c666c96fd5108d
fra199889 <55882910+fra199889@users.noreply.github.com>
2020-07-02 12:46:41 +0200

    APP FINITA!!!(NON é VERO,MA MI PIACE PENSARLO :) )

--- MarvelHeroes/app/src/main/java/it/tiburtinavalley/marvelheroes/fragment/FavouritesFragment.java
+++ MarvelHeroes/app/src/main/java/it/tiburtinavalley/marvelheroes/fragment/FavouritesFragment.java
@@ -129,10 +129,14 @@ public class FavouritesFragment extends Fragment implements MainActivity.IOnBack
 
         @Override
         public void onDestroyActionMode(ActionMode actionMode) {
-            return;
-        }
-
-    };
+           FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+           fragmentTransaction.setCustomAnimations(R.anim.anim_fade_in, R.anim.anim_fade_out);
+           HomeFragment home = new HomeFragment();
+           FavouritesFragment favorite = new FavouritesFragment();
+           fragmentTransaction.replace(R.id.fragment_container, favorite);
+           //fragmentTransaction.addToBackStack(null);   cosi da poter poi chiudere l'app direttamente se premuto back nella home
+            fragmentTransaction.commit();
+    };};
 
     @Override
     public void onDestroy() {
--- MarvelHeroes/app/src/main/java/it/tiburtinavalley/marvelheroes/recyclerviewadapter/FavoriteHeroAdapter.java
+++ MarvelHeroes/app/src/main/java/it/tiburtinavalley/marvelheroes/recyclerviewadapter/FavoriteHeroAdapter.java
@@ -175,7 +175,7 @@ public class FavoriteHeroAdapter extends SectionedRecyclerViewAdapter<FavoriteHe
         }
         if (holder.cl.isSelected()) {
 
-            holder.ivHeroPic.setColorFilter(R.color.selectionColor, PorterDuff.Mode.SRC_ATOP);
+            holder.ivHeroPic.setColorFilter(0x77000000, PorterDuff.Mode.SRC_ATOP);
         } else {
             holder.ivHeroPic.clearColorFilter();
         }
